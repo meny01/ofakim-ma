7c6b03a102119e66d7e01d61bca5839f7d5ab8b7
meny azulay <menyazol@outlook.com>
2021-01-18 03:08:26 +0200

    db

---
+++ ofakim-hw-ma/Migrations/20210118003849_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ofakim_hw_ma.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "exConvertEntities",
+                columns: table => new
+                {
+                    ExCorrencytId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExCorrencyName = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_exConvertEntities", x => x.ExCorrencytId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "exConvertEntities");
+        }
+    }
+}
